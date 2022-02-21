8f30a1f1ed037444dc37bcea4fdb38274b29375c
warmachine028 <warmachine028@outlook.com>
2022-02-22 01:21:35 +0530

    Added Pandas notebook

--- SEMESTER 4/Computer Organisation and Architecture/VHDL/3_SPECIAL_GATES/tests/testbench_xor.vhd
+++ SEMESTER 4/Computer Organisation and Architecture/VHDL/3_SPECIAL_GATES/tests/testbench_xor.vhd
@@ -11,8 +11,8 @@ architecture tb of testbench is
 -- DUT component
 component xor_gate is
 port(
-  A, B: in std_logic;
-  Y: out std_logic);
+	A, B: in std_logic;
+  	Y: out std_logic);
 end component;
 
 signal a_in, b_in, q_out: std_logic;
@@ -22,30 +22,30 @@ begin
 
   process
   begin
-    a_in <= '0';
-    b_in <= '0';
-    wait for 1 ns;
-    assert(q_out='0') report "Fail 0/0" severity error;
+	a_in <= '0';
+	b_in <= '0';
+	wait for 1 ns;
+	assert(q_out='0') report "Fail 0/0" severity error;
   
-    a_in <= '0';
-    b_in <= '1';
-    wait for 1 ns;
-    assert(q_out='1') report "Fail 0/1" severity error;
+	a_in <= '0';
+	b_in <= '1';
+	wait for 1 ns;
+	assert(q_out='1') report "Fail 0/1" severity error;
 
-    a_in <= '1';
-    b_in <= '0';
-    wait for 1 ns;
-    assert(q_out='1') report "Fail 1/0" severity error;
+	a_in <= '1';
+	b_in <= '0';
+	wait for 1 ns;
+	assert(q_out='1') report "Fail 1/0" severity error;
 
-    a_in <= '1';
-    b_in <= '1';
-    wait for 1 ns;
-    assert(q_out='0') report "Fail 1/1" severity error;
-    
-    -- Clear inputs
-    a_in <= '0';
-    b_in <= '0';
-    assert false report "Test done." severity note;
-    wait;
+	a_in <= '1';
+	b_in <= '1';
+	wait for 1 ns;
+	assert(q_out='0') report "Fail 1/1" severity error;
+	
+	-- Clear inputs
+	a_in <= '0';
+	b_in <= '0';
+	assert false report "Test done." severity note;
+	wait;
   end process;
 end tb;
