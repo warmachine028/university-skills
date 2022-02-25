a2d296b8fd66546c383c525a22bc55d1ec4592eb
Kryptonite <75939390+warmachine028@users.noreply.github.com>
2022-02-25 10:27:07 +0530

    Add files via upload

---
+++ Design n Analysis of Algorithm/WEEK 2/3_HeapSort.c
@@ -0,0 +1,20 @@
+// CODE 3: Heaps can be used in sorting an array. In max-heaps, maximum element will always be at the root. Heap Sort uses this property of heap to sort the array.
+
+#include <malloc.h>
+#include <stdio.h>
+
+void heapify()
+{
+}
+
+void heappush()
+{
+}
+
+void heappop()
+{
+}
+
+void heapSort() {
+
+}
