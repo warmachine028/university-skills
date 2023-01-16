03d18766d15468179d933540fe56941ec9fb5a80
Kryptonite <75939390+warmachine028@users.noreply.github.com>
2023-01-16 16:02:27 +0530

    Add files via upload

---
+++ Java/Assignments/WEEK1/Addition.java
@@ -0,0 +1,7 @@
+class Addition{
+    public static void main(String args[]){
+	int number1 = 10, number2 = 20;
+	int sum = number1 + number2;
+	System.out.println("Sum is " + sum);
+    }
+}
---
+++ Java/Assignments/WEEK1/Factorial2.java
@@ -0,0 +1,9 @@
+class Factorial2{
+    static int factorial(int n){
+	return n < 1 ? 1 : n * factorial(n - 1);
+    }
+    public static void main(String ...Args){
+	int number = 5;
+	System.out.println("Factorial of "+ number+ " is " + factorial(number));
+    }
+}
