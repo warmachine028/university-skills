365627c542f160e91bffb20b4b373a0e8beb5ebf
Kryptonite <75939390+warmachine028@users.noreply.github.com>
2023-01-18 19:37:43 +0530

    Update OddEven.java

--- Java/Assignments/WEEK1/OddEven.java
+++ Java/Assignments/WEEK1/OddEven.java
@@ -1,10 +1,10 @@
 class OddEven {
-    static boolean oddEven(int n) {
+    static boolean isEven(int n) {
         return n % 2 == 0;
     }
 
     public static void main(String... Args) {
         int number = 3;
-        System.out.println(number + " is " + (oddEven(number) ? "Even" : "Odd"));
+        System.out.println(number + " is " + (isEven(number) ? "Even" : "Odd"));
     }
 }
