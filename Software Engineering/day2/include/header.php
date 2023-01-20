4e23b95d714aaa7ba26ec831d9f97738d934fa8c
Kryptonite <75939390+warmachine028@users.noreply.github.com>
2023-01-20 15:13:36 +0530

    Add files via upload

---
+++ Software Engineering/day2/include/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <style>
+        table {
+            font-family: arial, sans-serif;
+            border-collapse: collapse;
+            width: 100%;
+        }
+
+        td,
+        th {
+            border: 1px solid #ffffff;
+            text-align: 'left';
+        }
+
+        tr:nth-child(){
+            background-color: #ffffff;
+        }
+    </style>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" zcontent="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello</title>
+</head>
+
+<body>
+    <h3>HTML Table</h3>
---
+++ Software Engineering/day2/index.php
@@ -0,0 +1,23 @@
+<?php
+include("include/header.php");
+?>
+<table>
+    <tr>
+        <th>Company</th>
+        <th>Contact</th>
+        <th>Country</th>
+    </tr>
+    <tr>
+        <td>Alfred Futterkiste</td>
+        <td>Maria Anders</td>
+        <td>Germany</td>
+    </tr>
+    <tr>
+        <td>Centro Commercial Moctezuma</td>
+        <td>Franchisco hang</td>
+        <td>Mexico</td>
+    </tr>
+</table>
+<?php
+include("include/footer.php");
+?>
