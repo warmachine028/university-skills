69a6b0f63e71178d8afe5fa4ae04494836a14ddd
Kryptonite <75939390+warmachine028@users.noreply.github.com>
2023-02-06 09:32:31 +0530

    Update isValidComment.c

--- Compiler Design/Assignments/WEEK1/isValidComment.c
+++ Compiler Design/Assignments/WEEK1/isValidComment.c
@@ -5,7 +5,7 @@ int isValidComment(char str[])
     char *newString = trim(str);
     int result = 0,
         length = strlen(newString);
-    if (strlen(newString) < 2) // Can't be shorter than 2
+    if (length < 2) // Can't be shorter than 2
         return result;
     if (newString[0] != '/') //  Should start with '/'
         return result;
