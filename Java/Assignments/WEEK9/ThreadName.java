999d739b5aa0382558bf554d60524502d8b3b16c
Pritam Kundu <warmachine028@outlook.com>
2023-04-17 10:41:15 +0530

    Update ThreadName.java

--- Java/Assignments/WEEK9/ThreadName.java
+++ Java/Assignments/WEEK9/ThreadName.java
@@ -1,13 +1,6 @@
 public class ThreadName {
     public static void main(String[] args) {
-        /*
-        Thread thread = new Thread("Good Thread"),
-                thread1 = new Thread("Bad Thread");
-        String name = thread.getName(), name1 = thread1.getName();
-
-        System.out.println(name + "\n" + name1);
-        */
         String name = Thread.currentThread().getName();
-        System.out.println(name);
+        System.out.println("Currently running thread is: " + name);
     }
 }
