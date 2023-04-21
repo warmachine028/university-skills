27d11184aeb3c66fc41ec501c305ebdb926c0cf2
warmachine028 <warmachine028@outlook.com>
2023-04-21 13:06:30 +0530

    feat: added Shell file

---
+++ .github/workflows/updateDate.sh
@@ -0,0 +1,10 @@
+DateSuffix() {       
+                case `date +%d` in     | 
+                  1|21|31) echo "st";; |
+                  2|22)    echo "nd";; |
+                  3|23)    echo "rd";; |
+                  *)       echo "th";; |
+                esac                   |
+              }                        |
+            newDate=`date "+    updated on: %d\`DaySuffix\` %B %Y"` |
+            sed -i "1s/ .* /$newDate/" README.md
