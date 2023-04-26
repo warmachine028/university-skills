f5907740132c3e7631d17b6157150689bd220090
Pritam Kundu <warmachine028@outlook.com>
2023-04-26 20:01:05 +0530

    Add files via upload

--- Java/Assignments/Midterm2/CountThread.java
+++ Java/Assignments/Midterm2/CountThread.java
@@ -1,1 +1,15 @@
+public class CountThread {
+	public static void main(String[] args) {
+		new Thread(() -> {
+				for (int i = 1; i <= 1000; i++)
+					System.out.println(i);
+		}).start();
+
+		new Thread(() -> {
+				for (int i = 1000; i >= 1; i--)
+					System.out.println(i);
+		}).start();
+	}
+}
+
 
