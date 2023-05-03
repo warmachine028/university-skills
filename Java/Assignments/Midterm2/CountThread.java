2650a9d017d93ba00af27ec3702eb4d9dcad18b8
warmachine028 <warmachine028@outlook.com>
2023-05-03 10:50:29 +0530

    feat: Added java Programs

--- Java/Assignments/Midterm2/CountThread.java
+++ Java/Assignments/Midterm2/CountThread.java
@@ -1,15 +1,13 @@
 public class CountThread {
 	public static void main(String[] args) {
 		new Thread(() -> {
-				for (int i = 1; i <= 1000; i++)
-					System.out.println(i);
+			for (int i = 1; i <= 1000; i++)
+				System.out.println(i);
 		}).start();
 
 		new Thread(() -> {
-				for (int i = 1000; i >= 1; i--)
-					System.out.println(i);
+			for (int i = 1000; i >= 1; i--)
+				System.out.println(i);
 		}).start();
 	}
 }
-
-
